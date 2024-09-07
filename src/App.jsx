b2c80290a48079c18d6daa9f90bcd7c4cdc04169
Sergio <alonzos181@gmail.com>
2024-09-07 22:07:25 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LuckyN from "./projects/Dice/LuckyN";
 import { sum } from "./projects/Dice/utils";

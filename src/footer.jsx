8e6daac5dd3e966292af4ea6076744e101c3ff2a
Sergio <alonzos181@gmail.com>
2025-02-23 11:25:03 +0000

    f

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Typography, IconButton, Container } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-
 const Footer = () => {
   return (
     <Box
@@ -11,7 +10,7 @@ const Footer = () => {
         py: 2,
         backgroundColor: (theme) =>
           theme.palette.mode === "light" ? "#f1f1f1" : "#2c2c2c",
-        boxShadow: "0 -2px 5px rgba(0, 0, 0, 0.1)", // Optional: Adds a subtle shadow
+        boxShadow: "0 -2px 5px rgba(0, 0, 0, 0.1)",
       }}
     >
       <Container sx={{ width: "10%", padding: 0, position: "absolute" }}>

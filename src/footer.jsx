da7710f32f3d4fd9eded8615fcfba6802cc583e6
Sergio <alonzos181@gmail.com>
2025-02-23 11:17:54 +0000

    fixing footer on projects

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, IconButton, Container } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 
 const Footer = () => {
   return (
     <Box
       component="footer"
       sx={{
-        // position: "relative",
-        // bottom: '-80px',
-        // left: 0,
-
-        width: "100%",
         py: 2,
-       
-        // flexShrink: 0,
-
         backgroundColor: (theme) =>
           theme.palette.mode === "light" ? "#f1f1f1" : "#2c2c2c",
         boxShadow: "0 -2px 5px rgba(0, 0, 0, 0.1)", // Optional: Adds a subtle shadow
       }}
     >
-      <Typography variant="body1" align="center">
-        © {new Date().getFullYear()} ESEALON
-      </Typography>
-      <Typography variant="body2" color="text.secondary" align="center">
-        All rights reserved.
-      </Typography>
+      <Container sx={{ width: "10%", padding: 0, position: "absolute" }}>
+        <IconButton href="https://github.com/esealon">
+          <GitHubIcon />
+        </IconButton>
+      </Container>
+      <Container sx={{}}>
+        <Typography variant="body1" align="center">
+          © {new Date().getFullYear()} ESEALON
+        </Typography>
+        <Typography variant="body2" color="text.secondary" align="center">
+          All rights reserved.
+        </Typography>
+      </Container>
     </Box>
   );
 };
